cba3ebf00ac7ee9b2b00e50290ca6d552db9a540
wykvictor <yakunw.victor@gmail.com>
2016-06-20 23:12:36 +0800

    add windows porting files

---
+++ windows/sys/time.h
@@ -0,0 +1,12 @@
+#ifndef GETTIME_H
+#define GETTIME_H
+
+// MSVC defines this in winsock2.h!?
+typedef struct timeval {
+    long tv_sec;
+    long tv_usec;
+} timeval;
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp);
+
+#endif
